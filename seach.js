d1e79cc73c239c55686f7896ae379a4ffe6e8507
Jonathan Lee <jonathanhlee8@gmail.com>
2022-09-02 01:44:46 -0700

    styling added, JS added for spotify fetch, API call works

---
+++ seach.js
@@ -0,0 +1,80 @@
+var btnContainer = document.querySelector(".artistBtnContainer");
+// var searchInput = document.querySelector("#search-bar");
+var searchBtn = document.querySelector("#searchbtn");
+
+function getParams() {
+
+  var searchParamsArr = document.location.search.split('&');
+
+  // Get the query and format values
+  var searchInputVal = searchParamsArr[0].split('=').pop();
+console.log(searchInputVal)
+  getToken(searchInputVal);
+}
+
+var getEvents = function (searchInput) {
+  var ticketMaster = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=aHO1boEvSw5W3eAvMn8m0d8RJpKAsYLH' + '&keyword=' + searchInput;
+
+  fetch(ticketMaster)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayEvents(data, searchInput);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to TicketMaster');
+    });
+};
+
+function getToken(input) {
+
+const clientID = "38e525a962894a2c817eec7482681b60";
+
+const clientSecret = "7dc693825518434f97548de1e223529b";
+
+
+fetch("https://accounts.spotify.com/api/token", {
+method:"POST",
+headers:{
+  'Content-Type':'application/x-www-form-urlencoded',
+  'Authorization':'Basic ' + btoa(clientID + ':' +clientSecret)
+},
+body:'grant_type=client_credentials'
+})
+.then(response=>{
+return response.json()
+})
+.then(data=>{
+getArtist(data.access_token, input)
+})
+};
+const getArtist = async (token, searchInput) => {
+// console.log(searchInput.value)
+const result = await fetch(`https://api.spotify.com/v1/search` +`?q=` + searchInput + `&type=track&limit=5`, {
+  method: 'GET',
+  headers: {'Authorization' : 'Bearer ' + token}
+});
+
+console.log(result)
+const data = await result.json();
+console.log(data)
+var trackArr = data.tracks.items
+
+
+trackArr.forEach(function(track) {
+var trackBtn = document.createElement("a")
+trackBtn.textContent = track.name
+trackBtn.setAttribute("href", track.external_urls.spotify )
+btnContainer.append(trackBtn)
+console.log(track.name)
+console.log(track.external_urls.spotify)
+console.log(track)
+})
+
+}
+
+getParams();
